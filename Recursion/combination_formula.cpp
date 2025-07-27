20077f17202f8c9d053e2d2f34aff97a0e6943d7
Jose Carlos Chaparro <61094548+JoseCarlosChaparro@users.noreply.github.com>
2025-07-27 14:12:19 -0600

    measuring execution time

--- Recursion/combination_formula.cpp
+++ Recursion/combination_formula.cpp
@@ -1,36 +1,35 @@
 #include <stdio.h>
+#include <time.h>
 
 double factorial(int n){
-    if(n>1)
-        return factorial(n-1)*n;
+    if(n > 1)
+        return factorial(n - 1) * n;
     else
         return 1;
 }
 
 double Ifactorial(int n){
-    int s=1;
-    int i;
-
-    for(i=1; i<=n; i++){
-        s=s*i;
+    int s = 1;
+    for(int i = 1; i <= n; i++){
+        s *= i;
     }
-
     return s;
 }
 
 double c(int n, int r){
-    double c;
-    c = factorial(n) / (factorial(r) * factorial(n-r));
-    return c;
+    double num, den;
+
+    num = factorial(n);
+    den = factorial(r)* factorial(n-r);
+    return num/den;
 }
 
-int C[10][10];
+int C[50][50];
 
 int cRecursive(int n, int r){
-    if(n == r || r==0 )
+    if(n == r || r == 0)
         return 1;
-    else
-        return cRecursive(n-1, r-1) + cRecursive(n-1,r);
+    return cRecursive(n - 1, r - 1) + cRecursive(n - 1, r);
 }
 
 int cRecursiveMemoization(int n, int r){
@@ -46,18 +45,39 @@ int cRecursiveMemoization(int n, int r){
     return C[n][r] = C[n - 1][r - 1] + C[n - 1][r];
 }
 
+void resetMemoTable(){
+    for(int i = 0; i < 50; i++)
+        for(int j = 0; j < 50; j++)
+            C[i][j] = -1;
+}
 
 int main(){
-    int r;
+    //TENER EN CUENTA EL OVERFLOW DE LAS FUNCIONES QUE RETORNAN INT
+    int n = 35, r = 17;
+    clock_t start, end;
+    double ms;
 
-    for(int i=0; i<10; i++){
-        for(int j=0; j<10; j++){
-            C[i][j] = -1;
-        }
-    }
+    // Tiempo c factorial
+    start = clock();
+    double res = c(n, r);
+    end = clock();
+    ms = (double)(end - start) * 1000.0 / CLOCKS_PER_SEC;
+    printf("c(%d,%d) = %lf, Time = %.3f ms\n", n, r, res, ms);
 
-    printf("%d\n", cRecursive(6,4));
-    printf("%d\n", cRecursiveMemoization(6,4));
-    
+    // Tiempo cRecursive
+    start = clock();
+    int res1 = cRecursive(n, r);
+    end = clock();
+    ms = (double)(end - start) * 1000.0 / CLOCKS_PER_SEC;
+    printf("cRecursive(%d,%d) = %d, Time = %.3f ms\n", n, r, res1, ms);
+
+    // Tiempo cRecursiveMemoization
+    resetMemoTable();
+    start = clock();
+    int res2 = cRecursiveMemoization(n, r);
+    end = clock();
+    ms = (double)(end - start) * 1000.0 / CLOCKS_PER_SEC;
+    printf("cRecursiveMemoization(%d,%d) = %d, Time = %.3f ms\n", n, r, res2, ms);
+
     return 0;
 }
